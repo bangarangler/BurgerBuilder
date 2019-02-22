819e087040cd890db0ed002ec00fce47714a731a
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-02-22 10:22:36 -0500

    Completed more of the lesson

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
 
 import classes from "./BurgerIngredient.module.scss";
 
-const burgerIngredient = props => {
+class  BurgerIngredient extends Component {
+  render () {
+
   let ingredient = null;
 
-  switch (props.type) {
+  switch (this.props.type) {
     case "bread-bottom":
       ingredient = <div className={classes.BreadBottom} />;
       break;
@@ -37,5 +40,10 @@ const burgerIngredient = props => {
 
   return ingredient;
 };
+}
 
-export default burgerIngredient;
+BurgerIngredient.propType = {
+  type: PropTypes.string.isRequired
+}
+
+export default BurgerIngredient;

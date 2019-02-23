c6dec8910675b4c7785dc1cfa6a4ca9e4dac4028
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-02-23 09:58:51 -0500

    Click Backdrop to dismiss backdrop and modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import classes from './Modal.module.scss';
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../Backdrop/Backdrop';
 
 const modal = props => {
   return (
+    <Aux>
+      <Backdrop show={props.show} clicked={props.modalClosed} />
   <div className={classes.Modal}
   style={{
     transform: props.show ? 'translateY(0)' : 'translateY(-100vh)', opacity: props.show ? '1' : '0'
@@ -10,6 +14,7 @@ const modal = props => {
   >
     {props.children}
   </div>
+  </Aux>
   )
 }
 

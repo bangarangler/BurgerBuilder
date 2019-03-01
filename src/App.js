ef9227afbfbecbe1c1c1716890b90d2d0f9f1bb4
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-03-01 17:56:40 -0500

    Created components for checkoutsummary and checkout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.module.scss";
 
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Layout>
           <BurgerBuilder />
+          <Checkout />
         </Layout>
       </div>
     );

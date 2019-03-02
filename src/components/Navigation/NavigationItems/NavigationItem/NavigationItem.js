f2ac76601e1159f54f403cfdc7e1a06e86332e93
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-03-02 11:54:12 -0500

    Dynamically displayed orderes from database

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import classes from './NavigationItem.module.scss';
+import { NavLink } from "react-router-dom";
+
+import classes from "./NavigationItem.module.scss";
 
 const navigationItem = props => (
-      <li className={classes.NavigationItem}>
-      <a 
-        href={props.link} 
-        className={props.active ? classes.active : null}>{props.children}
-        </a>
-        </li>
-)
+  <li className={classes.NavigationItem}>
+    <NavLink
+      exact={props.exact}
+      activeClassName={classes.active}
+      to={props.link}
+    >
+      {props.children}
+    </NavLink>
+  </li>
+);
 
 export default navigationItem;
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,19 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import classes from './NavigationItems.module.scss';
-import NavigationItem from './NavigationItem/NavigationItem';
+import classes from "./NavigationItems.module.scss";
+import NavigationItem from "./NavigationItem/NavigationItem";
 
 const navigationItems = props => {
   return (
     <ul className={classes.NavigationItems}>
-      <NavigationItem 
-        link="/" active>Burger Builder
+      <NavigationItem exact link="/">
+        Burger Builder
       </NavigationItem>
-      <NavigationItem link="/">
-       Checkout
-      </NavigationItem>
+      <NavigationItem link="/orders">Orders</NavigationItem>
     </ul>
-  )
-}
+  );
+};
 
 export default navigationItems;

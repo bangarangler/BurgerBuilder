f22c0f98b5e701fd231990a4782bc97133999845
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-03-02 17:05:57 -0500

    Added form Validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -18,7 +18,11 @@ class ContactData extends React.Component {
             type: "text",
             placeholder: "Your Name"
           },
-          value: ""
+          value: "",
+          validation: {
+            required: true
+          },
+          valid: false
         },
         street: {
           elementType: "input",
@@ -26,7 +30,11 @@ class ContactData extends React.Component {
             type: "text",
             placeholder: "Street"
           },
-          value: ""
+          value: "",
+          validation: {
+            required: true
+          },
+          valid: false
         },
         zipCode: {
           elementType: "input",
@@ -34,7 +42,13 @@ class ContactData extends React.Component {
             type: "text",
             placeholder: "Zipcode"
           },
-          value: ""
+          value: "",
+          validation: {
+            required: true,
+            minLength: 5,
+            maxLength: 5
+          },
+          valid: false
         },
         country: {
           elementType: "input",
@@ -42,7 +56,11 @@ class ContactData extends React.Component {
             type: "text",
             placeholder: "Country"
           },
-          value: ""
+          value: "",
+          validation: {
+            required: true
+          },
+          valid: false
         },
         email: {
           elementType: "email",
@@ -50,7 +68,11 @@ class ContactData extends React.Component {
             type: "text",
             placeholder: "Your Email"
           },
-          value: ""
+          value: "",
+          validation: {
+            required: true
+          },
+          valid: false
         },
         deliveryMethod: {
           elementType: "select",
@@ -92,6 +114,22 @@ class ContactData extends React.Component {
       });
   };
 
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+    return isValid;
+  }
+
   inputChangedHandler = (event, inputIdentifier) => {
     const updatedOrderForm = {
       ...this.state.orderForm
@@ -100,7 +138,12 @@ class ContactData extends React.Component {
       ...updatedOrderForm[inputIdentifier]
     };
     updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(
+      updatedFormElement.value,
+      updatedFormElement.validation
+    );
     updatedOrderForm[inputIdentifier] = updatedFormElement;
+    console.log(updatedFormElement);
     this.setState({ orderForm: updatedOrderForm });
   };
 

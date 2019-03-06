04a7711e6674b5744e836cedf49f37d0e0d48f03
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-03-06 08:35:09 -0500

    altered code with refactor to fix order button functionallity

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,7 +19,6 @@ class BurgerBuilder extends React.Component {
   //   }
   // }
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: false
@@ -45,7 +44,7 @@ class BurgerBuilder extends React.Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-    this.setState({ purchasable: sum > 0 });
+    return sum > 0;
   }
 
   purchaseHandler = () => {
@@ -97,7 +96,7 @@ class BurgerBuilder extends React.Component {
             ingredientAdded={this.props.onIngredientAdded}
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState(this.props.ings)}
             ordered={this.purchaseHandler}
             price={this.props.price}
           />

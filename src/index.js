fb3787ab6319b9f673c5954c3b28dc2e02b73bb6
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-03-08 16:45:44 -0500

    Added some more actions and hooked up more redux

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import * as serviceWorker from "./serviceWorker";
 
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
 import burgerBuilderReducer from "./store/reducers/burgerBuilder.js";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   burgerBuilderReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 ReactDOM.render(

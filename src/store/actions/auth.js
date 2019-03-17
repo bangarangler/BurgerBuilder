cb0b3bb661de4ee9da5b2cca9511572804957527
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-03-17 16:32:48 -0400

    Added Auth Sign in with Firebase and axios

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,27 +1,47 @@
-import * as actionTypes from './actionTypes.js'
+import axios from "axios";
+
+import * as actionTypes from "./actionTypes.js";
 
 export const authStart = () => {
   return {
     type: actionTypes.AUTH_START
-  }
-}
+  };
+};
 
-export const authSuccess = (authData) => {
+export const authSuccess = authData => {
   return {
     type: actionTypes.AUTH_SUCCESS,
     authData: authData
-  }
-}
+  };
+};
 
-export const authFail = (error) => {
+export const authFail = error => {
   return {
     type: actionTypes.AUTH_FAIL,
     error: error
-  }
-}
+  };
+};
 
 export const auth = (email, password) => {
   return dispatch => {
-    dispatch(authStart())
-  }
-}
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    axios
+      .post(
+        "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCkP2hgFAn91c_tsIa8ybxbe_NyPJLY-sQ",
+        authData
+      )
+      .then(res => {
+        console.log(res);
+        dispatch(authSuccess(res.data));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
+  };
+};

05fd538acb12ce5166b47f884caba48109eb5626
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-05-08 17:43:14 -0400

    Give user better options to sign up
    if they build a burger and redirect based on outcomes

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -4,7 +4,8 @@ import { updateObject } from "../utillity.js";
 const initialState = {
   ingredients: null,
   totalPrice: 4,
-  error: false
+  error: false,
+  building: false
 };
 
 const INGREDIENT_PRICES = {
@@ -26,7 +27,8 @@ const reducer = (state = initialState, action) => {
       );
       const updatedState = {
         ingredients: updatedIngredients,
-        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+        building: true
       };
       return updateObject(state, updatedState);
 
@@ -37,7 +39,8 @@ const reducer = (state = initialState, action) => {
       const updatedIngs = updateObject(state.ingredients, updatedIng);
       const updatedSt = {
         ingredients: updatedIngs,
-        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
+        building: true
       };
       return updateObject(state, updatedSt);
     case actionTypes.SET_INGREDIENTS:
@@ -49,7 +52,8 @@ const reducer = (state = initialState, action) => {
           meat: action.ingredients.meat
         },
         totalPrice: 4,
-        error: false
+        error: false,
+        building: false
       });
     case actionTypes.FETCH_INGREDIENTS_FAILED:
       return updateObject(state, { error: true });
